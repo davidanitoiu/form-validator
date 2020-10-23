d416412bd6db7ca9d492e535114856e5ef5d5538
David Anitoiu <david.anitoiu@gmail.com>
2020-10-23 14:10:19 +0200

    Replaced styled-components with emotion
    * styled-components was deprecated by it's developer

--- src/app/app-components.ts
+++ src/app/app-components.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "@emotion/styled";
 
 export const AppContainer = styled.div`
     background-color: #f9fafb;
--- src/components/form/form-components.tsx
+++ src/components/form/form-components.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "@emotion/styled";
 import { palette } from "utils/theme";
 
 export const Container = styled.div`
